1203a5ea6d75b899ff4101d091f813f566370c63
Ariel Guillermo Onore <a_onore@hotmail.com>
2022-04-02 20:04:24 -0300

    second commit

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/* Created by Ariel Guillermo Onore - Developer Trainee */
 document.querySelector('.menu-btn').addEventListener('click',() => {
 	document.querySelector('.nav-menu').classList.toggle('show');
 });
